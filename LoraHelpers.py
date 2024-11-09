8d9a00e0e446e40b9a1d327b1689989afe066fe6
Nootums <adi.sh751@gmail.com.com>
2024-11-09 14:48:48 +0530

    added hekpter to convert model to lora model, and change lora alpha

--- LoraHelpers.py
+++ LoraHelpers.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
+import transformers
 
 class LoraModule(nn.Module):
-    def __init__(self, orig_module:nn.modules.linear.Linear, r:int=8, alpha:int=1):
+    def __init__(self, orig_module:nn.modules.linear.Linear, r:int=8, alpha:float=1.0):
         assert type(orig_module)==nn.modules.linear.Linear, "Original module should be a Linear layer"
         assert type(r)==int, "r(rank) should be an integer"
-        assert type(alpha)==int, "alpha should be an integer"
+        assert type(alpha)==float, "alpha should be a float"
         super().__init__()
         self.alpha = alpha
         self.original_module = orig_module
@@ -21,8 +22,40 @@ class LoraModule(nn.Module):
         outs = self.original_module(x) + self.alpha*self.lora_module(x)
         return outs
     
-    def set_alpha(self, new_alpha:int):
-        assert type(new_alpha)==int, "New alpha value must be int"
+    def set_alpha(self, new_alpha:float):
+        assert type(new_alpha)==float, "New alpha value must be a float"
         self.alpha = new_alpha
         return
-    
+
+
+def convert_model_to_lora_model(model:transformers.modeling_utils.PreTrainedModel):
+    for name, module in model.named_parameters():
+        module.requires_grad = False
+        if module.ndim==1:
+            module.data = module.data.to(torch.float32)
+            continue
+        names = name.split('.')[:-1]
+        module_pointer = model
+        module_pointer_parent = None
+        for layer in names:
+            module_pointer_parent = module_pointer
+            module_pointer = getattr(module_pointer, layer)
+            if type(module_pointer)==LoraModule:
+                break
+        if type(module_pointer)==LoraModule:
+            continue
+        if type(module_pointer)==nn.modules.linear.Linear:
+            lora_module = LoraModule(module_pointer)
+            setattr(module_pointer_parent, names[-1], lora_module)
+    return
+
+def change_lora_alpha(model:transformers.modeling_utils.PreTrainedModel, new_alpha):
+    for name, module in model.named_parameters():
+        names = name.split('.')[:-1]
+        module_pointer = model
+        for layer in names:
+            module_pointer = getattr(module_pointer, layer)
+            if type(module_pointer)==LoraModule:
+                break
+        if type(module_pointer)==LoraModule:
+            module_pointer.set_alpha(new_alpha)
